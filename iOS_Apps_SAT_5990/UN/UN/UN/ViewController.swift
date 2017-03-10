3904c1b81196edf4753a8b76584d429cf1a3556a
sanlanka <slanka@mtu.edu>
2017-03-10 01:02:45 -0500

    Push Project UN

---
+++ iOS_Apps_SAT_5990/UN/UN/UN/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  UN
+//
+//  Created by Lanka on 13/02/17.
+//  Copyright © 2017 MTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+
+    func pri() {
+        self.pri()
+    print(email)
+    print(password)
+    }
+}
+
